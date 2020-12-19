7549c8a86e628a1569bf07a63317f9803b01091c
minh789 <58175316+minh789@users.noreply.github.com>
2020-12-19 17:01:17 +0700

    Add files via upload

--- TraSua.sql
+++ TraSua.sql
@@ -10,32 +10,55 @@ GO
 --Bill
 --DetailBill
 --Acount
---Barn 
---Materials
+--Ingredient 
 ----------------------------
 
-CREATE TABLE TableOfGuest
+CREATE TABLE OrderCard
 (
-	IdTable INT IDENTITY PRIMARY KEY ,
-	NameTable NVARCHAR (50) NOT NULL DEFAULT N'Bàn chưa có tên',
-	StatusTable NVARCHAR (50) NOT NULL DEFAULT N'Trống' --Trống || Có người 
+	CardId INT IDENTITY PRIMARY KEY,
+	CardStatus NVARCHAR (50) NOT NULL DEFAULT N'Thẻ trống' --Trống || Có người 
 )
 GO
 
-CREATE TABLE Menu
+CREATE TABLE Category
 (
-	IdCategory INT IDENTITY PRIMARY KEY,
-	NameCategory NVARCHAR (100) NOT NULL
+	CategoryId INT IDENTITY PRIMARY KEY,
+	CategoryName NVARCHAR (100) NOT NULL
 )
 GO
 
-CREATE TABLE MilkTea
+CREATE TABLE Drink
 (
-	IdMilkTea INT IDENTITY PRIMARY KEY ,
-	IdCategory  INT constraint FK_TK_Menu FOREIGN KEY (IdCategory ) REFERENCES  Menu(IdCategory) not null,
-	NameMilkTea NVARCHAR(50) NOT NULL DEFAULT N'Trà sữa truyền thống',
-	NameCategory NVARCHAR(50) NOT NULL,
+	DrinkId INT IDENTITY PRIMARY KEY ,
+	CategoryId  INT constraint FK_TK_Menu FOREIGN KEY (CategoryId ) REFERENCES  Category(CategoryId) not null,
+	DrinkName NVARCHAR(50) NOT NULL DEFAULT N'Trà sữa truyền thống',
 	Price FLOAT NOT NULL DEFAULT 0,
+)
+GO
+
+/*Topping NVARCHAR(50) NOT NULL,
+Ice INT NOT NULL,
+SUGAR INT NOT NULL*/
+
+
+CREATE TABLE Bill
+(
+	BillId INT IDENTITY PRIMARY KEY,
+	TableId  INT constraint FK_TK_Table FOREIGN KEY (TableId ) REFERENCES OrderCard (CardId) NOT NULL,
+	CheckInDate DATE NOT NULL DEFAULT GETDATE(),
+	ChekOutDate DATE NOT NULL DEFAULT GETDATE(),
+	BillStatus INT NOT NULL DEFAULT 0, --1:đã thanh toán ,--0:chưa thanh toán,
+	TotalPrice FLOAT NOT NULL,
+	
+)
+GO
+
+CREATE TABLE BillDetail
+(
+	BillDetailId INT IDENTITY PRIMARY KEY,
+	BillId  int constraint FK_TK_Bill FOREIGN KEY (BillId ) REFERENCES Bill (BillId) not null,
+	DrinkId  int constraint FK_TK_MilkTea FOREIGN KEY (DrinkId ) REFERENCES Drink (DrinkId) not null,
+	Number INT NOT NULL DEFAULT 0,
 	Size NVARCHAR(10) NOT NULL,
 	Topping NVARCHAR(50) NOT NULL,
 	Ice INT NOT NULL,
@@ -43,44 +66,12 @@ CREATE TABLE MilkTea
 )
 GO
 
-CREATE TABLE Bill
+CREATE TABLE Ingredient
 (
-	IdBill INT IDENTITY PRIMARY KEY,
-	IdTable  INT constraint FK_TK_Table FOREIGN KEY (IdTable ) REFERENCES TableOfGuest (IdTable) NOT NULL,
-	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
-	DateChekOut DATE NOT NULL DEFAULT GETDATE(),
-	StatusBill INT NOT NULL DEFAULT 0, --1:đã thanh toán ,--0:chưa thanh toán,
-	TotalPrice FLOAT NOT NULL
-)
-GO
-
-CREATE TABLE DetailBill
-(
-	IdDetailBill INT IDENTITY PRIMARY KEY,
-	IdBill  int constraint FK_TK_Bill FOREIGN KEY (IdBill ) REFERENCES Bill (IdBill) not null,
-	IdMilkTea  int constraint FK_TK_MilkTea FOREIGN KEY (IdMilkTea ) REFERENCES MilkTea (IdMilkTea) not null,
-	count INT NOT NULL DEFAULT 0 
-
-)
-GO
-
-CREATE TABLE Materials
-(
-	IdMaterials INT IDENTITY PRIMARY KEY,
-	NameMaterials NVARCHAR(100) NOT NULL,
-	BeginDate DATE NOT NULL DEFAULT GETDATE(),
+	IngredientID INT IDENTITY PRIMARY KEY,
+	IngredientName NVARCHAR(100) NOT NULL,
+	ProducedDate DATE NOT NULL DEFAULT GETDATE(),
 	ExpiryDate DATE NOT NULL DEFAULT GETDATE()
-)
-GO
-
-CREATE TABLE Barn
-(
-	NameBarn NVARCHAR(100) PRIMARY KEY,
-	IdMaterials  int constraint FK_TK_Materials FOREIGN KEY (IdMaterials ) REFERENCES Materials (IdMaterials) not null,
-	IdMilkTea  int constraint FK_TK_MilkTeaBarn FOREIGN KEY (IdMilkTea ) REFERENCES MilkTea (IdMilkTea) not null,
-	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
-	DateChekOut DATE NOT NULL DEFAULT GETDATE()
-	
 )
 GO
 
@@ -89,8 +80,63 @@ CREATE TABLE Account
 	UserName NVARCHAR(100) PRIMARY KEY,
 	DisplayName NVARCHAR(100) NOT NULL,
 	PassWord NVARCHAR(100) NOT NULL ,
-	Position INT NOT NULL ,--1:quản trị ,--2:quản lý,--3:nv quản lý kho,--4:nv quản lý hóa đơn ,--5:nv quản lý menu
+	Position NVARCHAR(100) NOT NULL ,--1:quản lý,--2:nv quản lý kho,--3:nv quản lý hóa đơn ,--4:nv quản lý menu
 )
 GO
 
+INSERT dbo.Account
+(
+    UserName,
+    DisplayName,
+    PassWord,
+    Position
+)
+VALUES
+(   N'kminh', -- UserName - nvarchar(100)
+    N'Khải Minh', -- DisplayName - nvarchar(100)
+    N'1701yeu2409', -- PassWord - nvarchar(100)
+    N'Quản lý'   -- Position - nvarchar(100)
+    )
+
+INSERT dbo.Account
+(
+    UserName,
+    DisplayName,
+    PassWord,
+    Position
+)
+VALUES
+(   N'hminh', -- UserName - nvarchar(100)
+    N'Huỳnh Minh', -- DisplayName - nvarchar(100)
+    N'minhdesigner', -- PassWord - nvarchar(100)
+    N'Quản lý Menu'   -- Position - nvarchar(100)
+    )
+
+INSERT dbo.Account
+(
+    UserName,
+    DisplayName,
+    PassWord,
+    Position
+)
+VALUES
+(   N'luan', -- UserName - nvarchar(100)
+    N'Thành Luân', -- DisplayName - nvarchar(100)
+    N'luanmegame', -- PassWord - nvarchar(100)
+    N'Quản lý kho'   -- Position - nvarchar(100)
+    )
+
+INSERT dbo.Account
+(
+    UserName,
+    DisplayName,
+    PassWord,
+    Position
+)
+VALUES
+(   N'quyen', -- UserName - nvarchar(100)
+    N'Trần Quyền', -- DisplayName - nvarchar(100)
+    N'quyencothu', -- PassWord - nvarchar(100)
+    N'Nhân viên'   -- Position - nvarchar(100)
+    )
 
---
+++ TruyXuatTayTay.sql
@@ -0,0 +1,68 @@
+﻿USE MilkTeaShop
+GO
+
+CREATE PROC USP_GetAccountByUserName	
+@username NVARCHAR(50)
+AS
+BEGIN
+	SELECT* FROM dbo.Account WHERE @username=UserName
+END
+GO
+
+EXEC dbo.USP_GetAccountByUserName @username = N'kminh' -- nvarchar(50)
+GO
+
+CREATE PROC USP_GetAccount	
+AS
+BEGIN
+	SELECT* FROM dbo.Account 
+END
+GO
+ 
+EXEC dbo.USP_GetAccount
+GO
+
+CREATE PROC USP_Login	
+@userName NVARCHAR(100),@passWord NVARCHAR(100), @position NVARCHAR(100)
+AS
+BEGIN
+	SELECT* FROM dbo.Account WHERE  @userName=UserName AND @passWord=PassWord  AND @position=Position
+END
+GO
+
+CREATE PROC USP_GetCardList
+AS
+BEGIN
+	SELECT* FROM dbo.OrderCard
+END
+GO
+
+EXECUTE dbo.USP_GetCardeList
+
+
+CREATE PROC CardUpdate(@n int)
+AS
+	BEGIN
+		DECLARE @i INT = 10;
+		WHILE @i<@n
+		BEGIN
+			INSERT dbo.OrderCard(CardId) VALUES (@i)
+			SET @i+=1
+		END
+	END
+
+		UPDATE dbo.TableOfGuest SET StatusTable = N'Đã có người' WHERE IdTable =9
+
+
+CREATE PROC USP_GetPos(@Username nvarchar(100), @pass nvarchar(100))
+AS
+	BEGIN
+		SELECT position
+		FROM dbo.Account
+		WHERE Username=@Username AND PassWord=@pass
+	END
+
+EXEC USP_GetPos N'hminh', N'minhdesigner' 
+
+EXEC USP_GetPos N'kminh', N'1701yeu2409' 
+
